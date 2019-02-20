9ba3fc52e90791a8af6357dca8256df0116790dd
Mario Gray <mario.gray@gmail.com>
2019-02-20 14:10:14 -0500

    JUNIT5 verifier

--- dc-02-19-19/producer/src/test/java/com/example/demo/BaseClass.java
+++ dc-02-19-19/producer/src/test/java/com/example/demo/BaseClass.java
@@ -2,27 +2,27 @@ package com.example.demo;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import reactor.core.publisher.Flux;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Import(WebConfig.class)
+@ExtendWith(SpringExtension.class)
 public class BaseClass {
-    @LocalServerPort
-    int port;
+  @LocalServerPort int port;
 
-    @MockBean
-    PersonRepository    personRepo;
+  @MockBean PersonRepository personRepo;
 
-    @BeforeEach
-    void setup() {
-        Mockito.when(personRepo.findAll())
-                .thenReturn(Flux.just(new Person("1234", "Bob")));
+  @BeforeEach
+  void setup() {
+    Mockito.when(personRepo.findAll()).thenReturn(Flux.just(new Person("1234", "Bob")));
 
-        RestAssured.baseURI = "localhost:" + port;
-    }
+    RestAssured.baseURI = "http://localhost:" + port;
+  }
 }
