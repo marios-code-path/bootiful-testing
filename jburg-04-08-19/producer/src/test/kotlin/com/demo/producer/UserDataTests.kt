e591a6d075208b8431b3c6970eb6fea74065e6e9
Mario Gray <mario.gray@gmail.com>
2019-04-08 09:50:30 +0200

    joburg 2

--- jburg-04-08-19/producer/src/test/kotlin/com/demo/producer/UserDataTests.kt
+++ jburg-04-08-19/producer/src/test/kotlin/com/demo/producer/UserDataTests.kt
@@ -1,9 +1,15 @@
 package com.demo.producer
 
+import junit.framework.Assert.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import reactor.core.publisher.Mono
+import reactor.test.StepVerifier
+import java.time.Instant
+import java.util.*
 
 @DataMongoTest
 @ExtendWith(SpringExtension::class)
@@ -11,7 +17,20 @@ class UserDataTests {
 
     @Test
     fun `should publish user`() {
+        val userMono = Mono
+                .just(User(UUID.randomUUID(), "Mario", Instant.now()))
 
+        StepVerifier
+                .create(userMono)
+                .expectSubscription()
+                .assertNext {
+                    assertAll("User State",
+                            { assertNotNull(it) },
+                            { assertEquals("Mario", it.name) },
+                            { assertTrue(Instant.now() > it.modified) })
+                }
+                .expectComplete()
+                .verify()
     }
 
 }
