62f46ff887e26c5eef94ddbecd6e3b016c76ab7b
Mario Gray <mario.gray@gmail.com>
2019-04-08 09:53:48 +0200

    joburg 3

--- jburg-04-08-19/producer/src/test/kotlin/com/demo/producer/UserDataTests.kt
+++ jburg-04-08-19/producer/src/test/kotlin/com/demo/producer/UserDataTests.kt
@@ -4,7 +4,9 @@ import junit.framework.Assert.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import reactor.core.publisher.Mono
 import reactor.test.StepVerifier
@@ -14,6 +16,9 @@ import java.util.*
 @DataMongoTest
 @ExtendWith(SpringExtension::class)
 class UserDataTests {
+
+    @Autowired
+    lateinit var repo: ReactiveMongoRepository<User, UUID>
 
     @Test
     fun `should publish user`() {
@@ -31,6 +36,20 @@ class UserDataTests {
                 }
                 .expectComplete()
                 .verify()
+    }
+
+    @Test
+    fun `should save and retrieve user`() {
+        val id = UUID.randomUUID()
+        val userMono = Mono
+                .just(User(id, "Mario", Instant.now()))
+
+        val opsPublisher = repo
+                .saveAll(userMono)
+                .then(
+                        repo.findById(id)
+                )
+
     }
 
 }
